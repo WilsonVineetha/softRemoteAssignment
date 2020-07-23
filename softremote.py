8fc5f128f4da34459a6e530afd41f14cf837265e
WilsonVineetha <67771877+WilsonVineetha@users.noreply.github.com>
2020-07-23 10:07:41 +0530

    Simple Softremote client code

--- softremote.py
+++ softremote.py
@@ -4,10 +4,12 @@ import time
 # Using Tkinter
 # import everything from tkinter module
 from tkinter import *
+
 # globally declare the expression variable
 expression = ""
 client_socket = socket.socket()  # instantiate 
 PORT_NUMBER = 27015
+
 
 # Function to update expression
 # in the text entry box
@@ -27,6 +29,7 @@ def press(num):
     data = client_socket.recv(64).decode()
     print('received:' + data)
     data = ''
+
 
 def connect(port):
     host = socket.gethostbyname('localhost')
@@ -53,6 +56,7 @@ def delay():
             elapsedTime = endTime - startTime
             print("Elapsed Time = %s" % elapsedTime)
 
+
 # Function to clear the contents
 # of text entry box
 def clear():
@@ -60,9 +64,9 @@ def clear():
     expression = ""
     equation.set("")
 
+
 # Driver code
 if __name__ == "__main__":
-
     # create a GUI window
     gui = Tk()
 
@@ -86,8 +90,8 @@ if __name__ == "__main__":
     # grid method is used for placing
     # the widgets at respective positions
     # in table like structure .
+    expression_field.pack(side=LEFT)
     expression_field.grid(columnspan=5, ipadx=100)
-
     equation.set('enter your choice')
 
     # create a Buttons and place at a particular
@@ -97,98 +101,107 @@ if __name__ == "__main__":
 
     CONNECT = Button(gui, text=' connect ', fg='black', bg='green',
                      command=lambda: connect(PORT_NUMBER), height=1, width=7)
-    CONNECT.grid(row=4, column=3)
+    CONNECT.grid(row=6, column=3)
+
+    L1 = Label(gui, text='Host/IP address')
+    L1.grid(row=4, column=3, ipadx=10)
+    HOST = Entry(gui, bd=5)
+    HOST.grid(row=4, column=4)
+
+    L2 = Label(gui, text='Port number')
+    L2.grid(row=5, column=3, ipadx=10)
+    PORT = Entry(gui, bd=5)
+    PORT.grid(row=5, column=4)
 
     DISCONNECT = Button(gui, text=' disconnect ', fg='black', bg='green',
-                     command=lambda: disconnect(), height=1, width=7)
-    DISCONNECT.grid(row=4, column=4)
+                        command=lambda: disconnect(), height=1, width=7)
+    DISCONNECT.grid(row=6, column=4)
 
-    SETTINGS = Label(gui, text=' Settings ')
-    SETTINGS.grid(row=5, column=3, ipadx=30)
+    SETTINGS = Label(gui, text=' Settings ', fg='white', bg='black')
+    SETTINGS.grid(row=7, column=3, ipadx=10)
 
-    L1 = Label(gui, text="Enter the delay")
-    L1.grid(row=6, column=3, ipadx=30)
+    L3 = Label(gui, text='Delay')
+    L3.grid(row=8, column=3, ipadx=10)
     DELAY = Entry(gui, bd=5)
-    DELAY.grid(row=6, column=4, ipadx=20)
+    DELAY.grid(row=8, column=4)
 
-    L2 = Label(gui, text="Enter the key value to be pressed")
-    L2.grid(row=7, column=3, ipadx=30)
+    L4 = Label(gui, text='Key')
+    L4.grid(row=9, column=3, ipadx=10)
     KEY_VALUE = Entry(gui, bd=5)
-    KEY_VALUE.grid(row=7, column=4, ipadx=20)
+    KEY_VALUE.grid(row=9, column=4)
 
-    L3 = Label(gui, text="Enter number of times the key to be fired")
-    L3.grid(row=8, column=3, ipadx=30)
+    L5 = Label(gui, text='Number of Times')
+    L5.grid(row=10, column=3, ipadx=10)
     NUM = Entry(gui, bd=5)
-    NUM.grid(row=8, column=4, ipadx=20)
+    NUM.grid(row=10, column=4)
 
     FIRE = Button(gui, text=' Fire ', fg='black', bg='green',
-                      command=lambda: delay(), height=2, width=7)
+                  command=lambda: delay(), height=2, width=7)
 
-    FIRE.grid(row=9, column=3 )
-
+    FIRE.grid(row=11, column=3)
 
     KEY1 = Button(gui, text=' 1 ', fg='black', bg='red',
-                     command=lambda: press(1), height=1, width=7)
+                  command=lambda: press(1), height=1, width=7)
     KEY1.grid(row=4, column=0)
 
     KEY2 = Button(gui, text=' 2 ', fg='black', bg='red',
-                     command=lambda: press(2), height=1, width=7)
+                  command=lambda: press(2), height=1, width=7)
     KEY2.grid(row=4, column=1)
 
     KEY3 = Button(gui, text=' 3 ', fg='black', bg='red',
-                     command=lambda: press(3), height=1, width=7)
+                  command=lambda: press(3), height=1, width=7)
     KEY3.grid(row=4, column=2)
 
     KEY4 = Button(gui, text=' 4 ', fg='black', bg='red',
-                     command=lambda: press(4), height=1, width=7)
+                  command=lambda: press(4), height=1, width=7)
     KEY4.grid(row=5, column=0)
 
     KEY5 = Button(gui, text=' 5 ', fg='black', bg='red',
-                     command=lambda: press(5), height=1, width=7)
+                  command=lambda: press(5), height=1, width=7)
     KEY5.grid(row=5, column=1)
 
     KEY6 = Button(gui, text=' 6 ', fg='black', bg='red',
-                     command=lambda: press(6), height=1, width=7)
+                  command=lambda: press(6), height=1, width=7)
     KEY6.grid(row=5, column=2)
 
     KEY7 = Button(gui, text=' 7 ', fg='black', bg='red',
-                     command=lambda: press(7), height=1, width=7)
+                  command=lambda: press(7), height=1, width=7)
     KEY7.grid(row=6, column=0)
 
     KEY8 = Button(gui, text=' 8 ', fg='black', bg='red',
-                     command=lambda: press(8), height=1, width=7)
+                  command=lambda: press(8), height=1, width=7)
     KEY8.grid(row=6, column=1)
 
     KEY9 = Button(gui, text=' 9 ', fg='black', bg='red',
-                     command=lambda: press(9), height=1, width=7)
+                  command=lambda: press(9), height=1, width=7)
     KEY9.grid(row=6, column=2)
 
     KEY0 = Button(gui, text=' 0 ', fg='black', bg='red',
-                     command=lambda: press(0), height=1, width=7)
+                  command=lambda: press(0), height=1, width=7)
     KEY0.grid(row=7, column=1)
 
     VOLUP = Button(gui, text=' VOLUP ', fg='black', bg='red',
-                  command=lambda: press(13), height=1, width=7)
+                   command=lambda: press(13), height=1, width=7)
     VOLUP.grid(row=8, column=0)
 
     VOLDOWN = Button(gui, text=' VOLDOWN ', fg='black', bg='red',
-                   command=lambda: press(12), height=1, width=7)
+                     command=lambda: press(12), height=1, width=7)
     VOLDOWN.grid(row=9, column=0)
 
     VOLMUTE = Button(gui, text=' VOLMUTE ', fg='black', bg='red',
-                      command=lambda: press(50), height=1, width=7)
+                     command=lambda: press(50), height=1, width=7)
     VOLMUTE.grid(row=3, column=2)
 
-    CHNLUP= Button(gui, text=' CHNLUP ', fg='black', bg='red',
+    CHNLUP = Button(gui, text=' CHNLUP ', fg='black', bg='red',
                     command=lambda: press(104), height=1, width=7)
     CHNLUP.grid(row=8, column=2)
 
     CHNLDOWN = Button(gui, text=' CHNLDOWN ', fg='black', bg='red',
-                    command=lambda: press(109), height=1, width=7)
+                      command=lambda: press(109), height=1, width=7)
     CHNLDOWN.grid(row=9, column=2)
 
     MENU = Button(gui, text=' MENU ', fg='black', bg='red',
-                    command=lambda: press(102), height=1, width=7)
+                  command=lambda: press(102), height=1, width=7)
     MENU.grid(row=11, column=0)
 
     RETURN = Button(gui, text=' RETURN ', fg='black', bg='red',
@@ -196,19 +209,19 @@ if __name__ == "__main__":
     RETURN.grid(row=9, column=1)
 
     INFO = Button(gui, text=' INFO ', fg='black', bg='red',
-                command=lambda: press(115), height=1, width=7)
+                  command=lambda: press(115), height=1, width=7)
     INFO.grid(row=11, column=2)
 
     OK = Button(gui, text=' OK ', fg='black', bg='red',
-                    command=lambda: press(28), height=1, width=7)
+                command=lambda: press(28), height=1, width=7)
     OK.grid(row=8, column=1)
 
     NAV_RIGHT = Button(gui, text=' NAVRIGHT ', fg='black', bg='red',
-                   command=lambda: press(106), height=1, width=7)
+                       command=lambda: press(106), height=1, width=7)
     NAV_RIGHT.grid(row=14, column=2)
 
     NAV_LEFT = Button(gui, text=' NAVLEFT ', fg='black', bg='red',
-                     command=lambda: press(105), height=1, width=7)
+                      command=lambda: press(105), height=1, width=7)
     NAV_LEFT.grid(row=14, column=0)
 
     NAV_UP = Button(gui, text=' NAVUP ', fg='black', bg='red',
@@ -216,27 +229,27 @@ if __name__ == "__main__":
     NAV_UP.grid(row=13, column=1)
 
     NAV_DOWN = Button(gui, text=' NAVDOWN ', fg='black', bg='red',
-                   command=lambda: press(108), height=1, width=7)
+                      command=lambda: press(108), height=1, width=7)
     NAV_DOWN.grid(row=15, column=1)
 
     FAST_FORWARD = Button(gui, text=' FAST_FORWARD ', fg='black', bg='red',
-                   command=lambda: press(208), height=1, width=7)
+                          command=lambda: press(208), height=1, width=7)
     FAST_FORWARD.grid(row=16, column=0)
 
     REWIND = Button(gui, text=' REWIND ', fg='black', bg='red',
-                          command=lambda: press(168), height=1, width=7)
+                    command=lambda: press(168), height=1, width=7)
     REWIND.grid(row=16, column=2)
 
     PLAY_PAUSE = Button(gui, text=' PLAY/PAUSE ', fg='black', bg='red',
-                          command=lambda: press(57), height=1, width=7)
+                        command=lambda: press(57), height=1, width=7)
     PLAY_PAUSE.grid(row=17, column=0)
 
     POWER = Button(gui, text=' POWER ', fg='black', bg='red',
-                 command=lambda: press(142), height=1, width=7)
+                   command=lambda: press(142), height=1, width=7)
     POWER.grid(row=2, column=2)
 
     FAV = Button(gui, text=' FAV ', fg='black', bg='red',
-                          command=lambda: press(49), height=1, width=7)
+                 command=lambda: press(49), height=1, width=7)
     FAV.grid(row=17, column=2)
 
     clear = Button(gui, text='Clear', fg='black', bg='red',
